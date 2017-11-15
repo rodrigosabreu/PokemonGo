095756f8da249bbee3a825f4b61a1dfeb8d4cab0
Rodrigo Abreu <rodrigoabreu@rodrigos-macbook-pro.local>
2017-11-15 15:14:34 -0200

    Criando PokeAgenda

---
+++ PokemonGo/PokeAgendaViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PokeAgendaViewController.swift
+//  PokemonGo
+//
+//  Created by Rodrigo Abreu on 15/11/2017.
+//  Copyright © 2017 Rodrigo Abreu. All rights reserved.
+//
+
+import UIKit
+
+class PokeAgendaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func voltarMapa(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
